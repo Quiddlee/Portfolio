edfa04b606f14e17e8356056936d69d444db16a5
Bogdan <thegarrybit@gmail.com>
2022-11-12 20:32:50 +0200

    some hovers added

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,12 +7,15 @@ const closeAside = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
+    hamburger.classList.add('hamburger_active');
 });
 
 closeElement.addEventListener('click', () => {
     menu.classList.remove('active');
+    hamburger.classList.remove('hamburger_active');
 });
 
 closeAside.addEventListener('click', () => {
     menu.classList.remove('active');
+    hamburger.classList.remove('hamburger_active');
 });
