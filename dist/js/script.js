696139a79b20d6df4159d378f1ed11f1deacf85a
Bogdan <thegarrybit@gmail.com>
2022-11-12 19:24:15 +0200

    hamburger menu done

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeElement = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElement.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
