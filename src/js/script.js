495379e3dd5977448c9dbd8fe752abd39a931434
Bogdan <thegarrybit@gmail.com>
2022-11-12 19:41:43 +0200

    hamburger outside click close added

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,18 @@
+//hamburger
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElement = document.querySelector('.menu__close');
+const closeAside = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeElement.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+closeAside.addEventListener('click', () => {
     menu.classList.remove('active');
 });
